3bcbe50588649b0c4e1d5d39b4c90cc4de3c40fe
Aakee <akseli.konttas@gmail.com>
2024-12-01 13:04:05 +0200

    Added template

---
+++ aoc2024-template.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+'''
+=====
+ABOUT
+=====
+Solution for the Advent of Code 2024, day XX challenge.
+More information from the official website: https://adventofcode.com/2024
+
+==========
+HOW TO RUN
+==========
+You can run the script by calling
+    [path-to-python-intepreter] [path-to-this-script] [path-to-input-text-file]
+For example:
+    python3 ./aoc2023-day01.py ./inputs/day01.txt
+
+Alternatively, you can modify the FILENAME constant at the beginning of the script to give the path to the input file.
+If both command line argument and hard-coded filename is given, the command line argument takes precedence.
+
+======
+GIT REPOSITORY
+======
+The git repository for my solutions for AoC 2024 challenges can be found here: https://github.com/Aakee/AoC-2024
+'''
+
+import argparse
+import collections
+import itertools
+import logging
+import math
+
+LOGLEVEL = logging.WARNING
+logging.basicConfig(level=LOGLEVEL, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+# Type the path to your input text file here if you do not wish to use the command line argument
+FILENAME = "./inputs/dummy.txt"
+
+
+def load_file(fn: str):
+    '''
+    Function loads the contents of the given text file and returns the contents as a list.
+    @param fn:      path to the file to-be-loaded
+    @returns:       contents of the file as a list, one entry corresponding to one line
+    '''
+    with open(fn,'r') as file:
+        ret = ''
+        for line in file:   # Loop through the file and add the lines to the return value
+            ret += line
+    return ret.split('\n')
+
+# =========================
+
+
+
+
+
+# =========================
+
+def part1(data: list) -> int:
+    '''
+    Solution for the part 1.
+    '''
+    ans = 0
+
+    return ans
+
+
+def part2(data: list) -> int:
+    '''
+    Solution for the part 2.
+    '''
+    ans = 0
+
+    return ans
+
+# =========================
+
+if  __name__ == "__main__":
+    # Fetch input text file path, either from the hard-coded variable FILENAME or from command line argument (if given)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_fn', nargs='?', default=FILENAME, help="Path to the input text file. Optional; if not given, will default to the one hard-coded in the beginning of the script file.")
+    args =  parser.parse_args()
+
+    # Load the data
+    data = load_file(args.input_fn)
+
+    # Execute and print the solutions
+    print(f"Part 1 solution: {part1(data)}")
+    print(f"Part 2 solution: {part2(data)}")
